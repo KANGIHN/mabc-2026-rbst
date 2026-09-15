244f7afbb8250558ba2b83dbce9747586b9472df
goyo <robust2991@gmail.com>
2026-09-15 19:33:42 +0900

    Delete api/test.js

--- api/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  return res.status(200).json({
-    ok: true,
-    message: "테스트 API 동작 중",
-    envUpstageKeyPresent: !!process.env.UPSTAGE_API_KEY,
-    nodeVersion: process.version
-  });
-};
